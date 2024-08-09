57651ed3ebfda06b426581c1bab737e3e899e054
Adam Gagorik <adam.gagorik@gmail.com>
2024-08-08 21:00:02 -0400

    Add --version command

--- mdsphinx/__main__.py
+++ mdsphinx/__main__.py
@@ -1,5 +1,6 @@
 import logging
 
+from typer import Exit
 from typer import Typer
 
 import mdsphinx.core.environment
@@ -7,20 +8,26 @@ import mdsphinx.core.prepare
 import mdsphinx.core.process
 
 
-app = Typer(add_completion=False, rich_markup_mode="rich")
-
-
-@app.callback()
-def cb(verbose: bool = False) -> None:
-    """
-    Convert markdown to any output format that Sphinx supports.
-    """
-    logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO, format="%(levelname)-8s | %(message)s")
+app = Typer(add_completion=False, rich_markup_mode="rich", invoke_without_command=True)
 
 
 app.add_typer(mdsphinx.core.environment.app, name="env")
 app.command(epilog=mdsphinx.core.prepare.EPILOG.replace("\n", "\n\n"))(mdsphinx.core.prepare.prepare)
 app.command(epilog=mdsphinx.core.process.EPILOG.replace("\n", "\n\n"))(mdsphinx.core.process.process)
+
+
+@app.callback()
+def cb(version: bool = False, verbose: bool = False) -> None:
+    """
+    Convert markdown to any output format that Sphinx supports.
+    """
+    if version:
+        from mdsphinx import __version__
+
+        print(__version__)
+        raise Exit(0)
+
+    logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO, format="%(levelname)-8s | %(message)s")
 
 
 if __name__ == "__main__":
